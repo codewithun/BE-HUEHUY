e51f9c448d034e21d12fce9aa344c599d39ebf33
Nur Ridwan <nurridwan270305@gmail.com>
2025-11-25 09:59:46 +0700

    refactor(DashboardController): improve code formatting and include HuehuyAd count in response

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,28 +9,29 @@ use App\Models\Cube;
 use App\Models\User;
 use App\Models\World;
 use App\Models\Community;
+use App\Models\HuehuyAd;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function counterData()
     {
-        $cubes = Cube::count('*');
-        $ads = Ad::count('*');
-
-        $corporates = Corporate::count('*');
-        $users = User::count('*');
-
+        $cubes       = Cube::count('*');
+        $ads         = Ad::count('*');
+        $huehuyAds   = HuehuyAd::count('*');
+        $corporates  = Corporate::count('*');
+        $users       = User::count('*');
         $communities = Community::count('*');
 
         return response([
             'message' => 'Success',
             'data' => [
-                'cubes' => $cubes,
-                'ads' => $ads,
-                'corporates' => $corporates,
-                'users' => $users,
-                'communities' => $communities,
+                'cubes'        => $cubes,
+                'ads'          => $ads,
+                'huehuy_ads'   => $huehuyAds,
+                'corporates'   => $corporates,
+                'users'        => $users,
+                'communities'  => $communities,
             ]
         ]);
     }

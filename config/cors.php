fea54eee21bc16264e8e1a70257b0253727bcabc
FzNrfdhl <68339451+Faiznrfdhl@users.noreply.github.com>
2026-04-22 13:55:25 +0700

    fix authentication & image display

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,8 @@ return [
     'https://app-159-223-48-146.nip.io',
     'http://localhost:3000',
     'http://localhost:5173',
+    'http://192.168.56.1:3000',
+    'http://192.168.56.1:9000',
   ],
 
   'allowed_origins_patterns' => [],
